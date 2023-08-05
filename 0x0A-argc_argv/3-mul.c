d53ffe2aa9fffe29a4800aae55505852bf03c91a
youssefmagdy5 <youssef.magdymikhail55@gmail.com>
2023-08-04 18:36:30 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,13 +6,13 @@
 * @argv :array
 * Return 0-success, non-zero-fail
  */
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
 	int sum;
 	if (argc == 3)
 	{
 		sum= atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", sum)
+		printf("%d\n", sum);
 	}
 	else
 	{
