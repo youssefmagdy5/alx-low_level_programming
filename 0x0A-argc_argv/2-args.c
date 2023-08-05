d7be04b9258b7cdcae435dd6da8a20a6af77f8f7
youssefmagdy5 <youssef.magdymikhail55@gmail.com>
2023-08-04 18:13:52 -0700

    22

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints the number
+* @argc :int
+* @argv :list
+* Return :0
+ */
+int main(int argc ,char const **argv[])
+{	int i = 0;
+	while (argc--)
+{
+printf("%s\n" ,argv[i]);
+i++;
+}
+return (0);
+}
---
+++ 0x0A-argc_argv/3-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints sum of 2 no
+* @argc :int
+* @argv :array
+* Return 0-success, non-zero-fail
+ */
+int main(int argc, char **argv[])
+{
+	int sum;
+	if (argc == 3)
+	{
+		sum= atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum)
+	}
+	else
+	{
+		printf("error\n");
+			return (1);
+	}
+return (0);
+}
