7cf2b44a488e461d304506b119d7b19a5d5451ed
youssefmagdy5 <youssef.magdymikhail55@gmail.com>
2023-08-14 08:04:51 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,17 @@
+#ifndef DOG_H
 #define DOG_H
+
 /**
 * struct dog -hhh
 * @name: name
 * @age: age
 * @owner: owner
 * Description: lll
- */
+*/
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+#endif
