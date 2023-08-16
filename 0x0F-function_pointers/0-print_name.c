c4e482556d83ee799d08e7e3bf8509cf6f8457e4
youssefmagdy5 <youssef.magdymikhail55@gmail.com>
2023-08-16 06:32:37 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* print_name - funn
+* @name: string
+* @f: pointer
+* Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
