bc0a830d67e8e279e2c6adc5b1f02db7f7e94395
youssefmagdy5 <youssef.magdymikhail55@gmail.com>
2023-08-26 09:22:49 -0700

    sk

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
 * main - prints sum of 2 no
-* @argc :int
-* @argv :array
-* Return 0-success, non-zero-fail
+* @argc: int
+* @argv: array
+* Return: 0-success, non-zero-fail
  */
 int main(int argc, char *argv[])
 {
 	int sum;
+
 	if (argc == 3)
 	{
-		sum= atoi(argv[1]) * atoi(argv[2]);
+		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
 	}
 	else
