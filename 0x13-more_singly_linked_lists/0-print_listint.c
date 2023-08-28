95b390be1ebf98e7880dab4c01cd3aefb6567a6a
youssefmagdy5 <youssef.magdymikhail55@gmail.com>
2023-08-28 11:15:24 -0700

    finished

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -19,30 +19,3 @@ size_t print_listint(const listint_t *h)
 
  return (num);
 }
-
-
-==========================
-
-1-listint_len.c
-
-
-#include "lists.h"
-
-/**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of nodes
- */
-size_t listint_len(const listint_t *h)
-{
- size_t num = 0;
-
- while (h)
- {
- num++;
- h = h->next;
- }
-
- return (num);
-}
